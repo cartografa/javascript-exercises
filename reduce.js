bc8d1abb73190c2b7c0319ffcb0c1a79d9b58a6c
Carolina S. M <carostmaria@gmail.com>
2021-06-11 18:43:56 -0300

    add more examples of reduce

--- reduce.js
+++ reduce.js
@@ -19,10 +19,9 @@ const topScore = notas.reduce((max, valorActual) => {
     if (valorActual > max) return valorActual;
     return max;
 })
-topScore;
 
 //implicit return usando math.max:
-const topScore = notas.reduce((max, valorActual) => (
+const topScore2 = notas.reduce((max, valorActual) => (
     Math.max(max, valorActual)
 ))
 
@@ -35,3 +34,59 @@ const minScore = notas.reduce((min, valorActual) => (
 const suma = [10, 20, 30].reduce((sum, valorActual) => {
     return sum + valorActual;
 }, 1000) 
+
+
+//reduce for tallying
+
+//ejemplo: contar votos
+const votos = ['sí', 'sí', 'no', 'sí', 'no', 'sí', 'sí', 'sí', 'no', 'no', 'sí', 'sí', 'sí', 'no', 'sí', 'no', 'sí', 'sí', 'no'];
+
+/* const resultados = votos.reduce((tally, value) => {
+    if(tally[value]) {
+        tally[value]++;
+    } else {
+        tally[value] = 1;
+    }
+    return tally;
+    }, {})  */
+
+const resultados = votos.reduce((tally, value) => {
+    tally[value] = (tally[value] || 0) + 1;
+    // si existe suma 1 al valor, si está undefined
+    // o sea: 0, suma 1 también.
+    return tally;
+}, {})
+
+//agrupar libros por rating:
+let books = [{
+    title: 'En medio de Spinoza', 
+    authors: ['Gilles Deleuze'],
+    rating: 4.65
+},
+{
+    title:  'La mano izquierda de la oscuridad',
+    authors: ['Ursula K. Le Guin'],
+    rating: 4.89
+},
+{
+    title: 'Mil Mesetas',
+    authors: ['Gilles Deleuze', 'Felix Guattari'],
+    rating: 4.8
+},
+{
+    title:  'Curso sobre la percepción',
+    authors: ['Gilbert Simondon'],
+    rating: 3.5
+}
+]
+
+const groupedByRatings = books.reduce((groupedBooks, book) => {
+    const key = Math.floor(book.rating);
+    if (!groupedBooks[key]) groupedBooks[key] = [];
+    groupedBooks[key].push(book)
+    return groupedBooks
+}, {})
+
+
+
+
