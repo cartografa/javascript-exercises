8d5e6ad29845586fccec53da1ea41e296aba308e
Carolina S. M <carostmaria@gmail.com>
2021-06-16 17:01:04 -0300

    delete duplicated files

--- function1.js
+++
@@ -1,8 +0,0 @@
-function roll(numRolls) {
-    for (let i = 0; i < numRolls; i++) {
-        let r = Math.floor(Math.random() * 6 + 1);
-        console.log (`Rolled: ${r}`);
-    }
-}
-
-roll(3);
--- function4.js
+++
@@ -1,8 +0,0 @@
-function average (arr) {
-    for (let num of arr) {
-        total += num;
-    }
-    return total / arr.lenght;
-}
-
-average([0, 50]);
