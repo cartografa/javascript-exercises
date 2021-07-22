cffbba409b234b20c71d0ccf7d44595a35cb53ba
Carolina S. M <carostmaria@gmail.com>
2021-07-22 17:27:10 -0300

    Refactor with class keyword

--- examples/constructorFunction.js
+++ examples/constructorFunction.js
@@ -1,23 +1,57 @@
- function Color(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
-};
+// CONSTRUCTOR FUNCTION
 
-Color.prototype.rgb = function() {
-    const {r, g, b} = this;
-    return `rgb(${r}, ${g}, ${b})`;
-};
+//  function Color(r, g, b) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+// };
 
-Color.prototype.hex = function() {
-    const {r, g, b} = this;
-    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
+// Color.prototype.rgb = function() {
+//     const {r, g, b} = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+// };
 
-Color.prototype.rgba = function(a = 1.0) {
-    const {r, g, b} = this;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
+// Color.prototype.hex = function() {
+//     const {r, g, b} = this;
+//     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function(a = 1.0) {
+//     const {r, g, b} = this;
+//     return `rgba(${r}, ${g}, ${b}, ${a})`;
+// }
+
+// NEW OPERATOR!
+
+// const color1 = new Color(255, 40, 100);
+// const color2 = new Color(0, 0, 0);
+
+
+// ************************************** //
+//  BETTER WAY: JS CLASS SYNTACTIC SUGAR  //
+// ************************************** //
+
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+    innerRGB() {
+        const {r, g, b} = this;
+        return `${r}, ${g}, ${b}`;
+    }
+    rgb() {
+        return `rgb(${this.innerRGB()})`;
+    }
+    rgba(a = 1.0) {
+        return `rgba(${this.innerRGB}, ${a})`;
+    }
+    hex() {
+        const {r, g, b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
 }
 
-const color1 = new Color(255, 40, 100);
-const color2 = new Color(0, 0, 0);
+const c1 = new Color(255, 67, 89, 'tomato');
